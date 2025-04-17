9e6049b5f4a4df83996b3c4f5ed4536c6e463d53
Ferhat Yiğit Aydın <ferhat_yigit_aydin@hotmail.com>
2025-04-17 17:31:24 +0300

    front düzenleme

--- MyApplication.Web/obj/Release/net6.0/MyApplication - Backup.Web.AssemblyInfo.cs
+++ MyApplication.Web/obj/Release/net6.0/MyApplication - Backup.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication - Backup.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b85a5d3e45697d3bf6c3662ae95e517e667adc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3153a5e5c4dc8ba326a58514d8ad1cab2bbc76")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication - Backup.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication - Backup.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
